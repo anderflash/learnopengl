34d584ab5433702cfb430c86bbc99dbc1244d208
Anderson Tavares <nocturne.pe@gmail.com>
2015-11-11 18:04:47 -0200

    Versão com EBO

--- main.c
+++ main.c
@@ -105,16 +105,26 @@ int main(void)
   glBindVertexArray(VAO);
 
   GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    0.5f,  0.5f, 0.0f,  // Top Right
+    0.5f, -0.5f, 0.0f,  // Bottom Right
+   -0.5f, -0.5f, 0.0f,  // Bottom Left
+   -0.5f,  0.5f, 0.0f   // Top Left
   };
+  GLuint indices[] = {
+    0, 1, 3,
+    1, 2, 3
+  };
+
   GLuint VBO;
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
 
+  GLuint EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   GLuint shaderProgram;
   init_shaders(&shaderProgram);
@@ -130,7 +140,10 @@ int main(void)
     glClear(GL_COLOR_BUFFER_BIT);
 
     glEnableVertexAttribArray(0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
     glDisableVertexAttribArray(0);
 
     glfwSwapBuffers(window);
